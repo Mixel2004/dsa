e7935f7e8ea281104f85a0902e731dc2669c221d
Mixel2004 <84668201+Mixel2004@users.noreply.github.com>
2022-10-17 23:21:17 +0530

    Create 88_Merge_Sorted_Array.cpp

---
+++ 88_Merge_Sorted_Array.cpp
@@ -0,0 +1,30 @@
+//https://leetcode.com/problems/merge-sorted-array/
+
+class Solution {
+public:
+    void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
+        int i = 0, j = 0;
+        vector<int> ans;
+        while(i < m && j < n) {
+            if(nums1[i] < nums2[j]) {
+                ans.push_back(nums1[i++]);
+            } else {
+                ans.push_back(nums2[j++]);
+            }
+        }
+        if(i < m) {
+            while(i<m) {
+                ans.push_back(nums1[i++]);
+            }
+        } else {
+            while(j<n) {
+                ans.push_back(nums2[j++]);
+            }
+        }
+        int k = 0;
+        while(k < ans.size()) {
+            nums1[k] = ans[k];
+            k++;
+        }
+    }
+};
