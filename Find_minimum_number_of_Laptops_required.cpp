7ad0feb80e098bce07ea71f0d01c1803f0738e10
Mixel2004 <84668201+Mixel2004@users.noreply.github.com>
2023-01-01 11:54:06 +0530

    Create Find_minimum_number_of_Laptops_required.cpp

---
+++ Find_minimum_number_of_Laptops_required.cpp
@@ -0,0 +1,49 @@
+//{ Driver Code Starts
+// Initial Template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+//User function Template for C++
+
+class Solution {
+  public:
+    int minLaptops(int N, int start[], int end[]) {
+        sort(start, start + N);
+        sort(end, end + N);
+        int i = 0, j = 0, ans = 0, curr = 0;
+        while (i < N && j < N) {
+            if (start[i] < end[j]) {
+                curr++;
+                i++;
+            } else {
+                curr--;
+                j++;
+            }
+            ans = max(ans, curr);
+        }
+        return ans;
+    }
+};
+
+//{ Driver Code Starts.
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int N;
+        cin >> N;
+        int start[N], end[N];
+        for(int i=0; i<N; i++)
+            cin>>start[i];
+        for(int i=0; i<N; i++)
+            cin>>end[i];
+            
+        Solution ob;
+        cout << ob.minLaptops(N, start, end) << endl;
+    }
+}
+// } Driver Code Ends
