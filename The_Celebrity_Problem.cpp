db2ff88a792af408b7817120cf0baa2996831d8c
Mixel2004 <84668201+Mixel2004@users.noreply.github.com>
2023-03-12 01:51:41 +0530

    feat: Added a better solution

--- The_Celebrity_Problem.cpp
+++ The_Celebrity_Problem.cpp
@@ -7,6 +7,39 @@ using namespace std;
 // } Driver Code Ends
 //User function template for C++
 
+class Solution 
+{
+    public:
+    //Function to find if there is a celebrity in the party or not.
+    int celebrity(vector<vector<int> >& M, int n) 
+    {
+        stack<int> s;
+        for(int i = 0; i < n; i++) s.push(i);
+        
+        while(s.size() > 1) {
+            int a = s.top();
+            s.pop();
+            int b = s.top();
+            s.pop();
+            
+            if(M[a][b]) {
+                s.push(b);
+            } else {
+                s.push(a);
+            }
+        }
+        
+        int ans = s.top();
+        int sum = 0;
+        for(auto it: M[ans]) sum+= it;
+        if(sum != 0) return -1;
+        for(int i = 0; i < n; i++) sum += M[i][ans];
+        if(sum != n-1) return -1;
+        return ans;
+        
+    }
+};
+
 class Solution2 
 {
     public:
